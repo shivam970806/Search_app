dc999d2db3d0448a204f78f2faec68031563ce65
shivam <rs2000430gmail.com>
2021-09-01 20:39:49 +0530

    Third commit

--- TodayProj/settings.py
+++ TodayProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bwy*6pqn$(fva_nijlf*n7#)lubc_12adyb5m(k#il6fjh$t)&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','searchingapps.herokuapp.com']
 
 
 # Application definition
